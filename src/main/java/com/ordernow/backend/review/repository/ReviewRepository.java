b49c2a80de7d0af9fb688de00a73d2349a3236e1
owen0806 <owen080614@gmail.com>
2025-01-16 11:44:57 +0100

    Update new api to fix problem

--- src/main/java/com/ordernow/backend/review/repository/ReviewRepository.java
+++ src/main/java/com/ordernow/backend/review/repository/ReviewRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
     List<Review> findAllByStoreId(String storeId);
     Page<Review> findAllByStoreId(String storeId, Pageable pageable);
     int countByStoreId(String storeId);
+    Optional<Object> findByOrderId(String orderId);
 }

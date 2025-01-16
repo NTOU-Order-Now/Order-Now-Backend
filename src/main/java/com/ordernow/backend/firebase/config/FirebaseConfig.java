24fb609d6b9c3d9b2622ea5d67a0d048fcc4dde2
owen0806 <owen080614@gmail.com>
2025-01-16 12:26:48 +0100

    Add firebase config

---
+++ src/main/java/com/ordernow/backend/firebase/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.ordernow.backend.firebase.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount
+                    = new FileInputStream("./serviceAccountKey.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
